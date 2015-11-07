85bab67d953f5cef719aa1c0434488fcc87ff41d
Julien BOUVIER-VOLAILLE <bouviervj@Irinas-MacBook-Air.local>
2015-11-07 17:30:51 -0500

    Added description first code for sqlitedb

--- src/main/java/mastercard/d1/business/Charity.java
+++ src/main/java/mastercard/d1/business/Charity.java
@@ -1,6 +1,9 @@
 package mastercard.d1.business;
 
+import java.sql.Connection;
 import java.util.Vector;
+
+import mastercard.d1.db.SqliteDBConnect;
 
 public class Charity {
 
@@ -9,15 +12,19 @@ public class Charity {
 	Vector<String> categories;
 	String description;
 	String public_key;
+	String private_key;
 	
 	public static Vector<Charity> retrieveCharityByCategoryAndZipcode(String iCategory, String iZipcode){
 		
 		Vector<Charity> charities = new Vector<Charity>();
 		
+		Connection connect = SqliteDBConnect.getConnection();
+		
 		Charity charity = new Charity();
 		charity.name = "Veteran - Avon Hill";
 		charity.logoPath = "static/images/AvonHillVeteran.gif";
 		charity.categories = new Vector<String>();
+		charity.description = "This is the description";
 		charity.categories.add("VETERAN");
 		charity.public_key = "ABCDEF";
 		
